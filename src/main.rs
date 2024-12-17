be69a22eb119ba2b56fb13b1f385fd6e150d5b0a
Mathias de Riese <mdrie@fastmail.com>
2024-12-17 22:35:03 +0100

    Using `clap`.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+// Idenfify backup copies to be deleted according to a given retention policy.
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The pattern to look for
+    #[arg(short, long, default_value = "")]
+    regex: String,
+    /// The path to the file to read
+    #[arg(short, long, default_value = ".")]
+    path: std::path::PathBuf,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("{:?}", args);
 }
